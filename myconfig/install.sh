ca57d72575eaa2583f590884f8dfd39b79c23414
univ <univ@univ-VirtualBox.(none)>
2013-08-27 11:20:31 +0800

    add tmux conf

--- myconfig/install.sh
+++ myconfig/install.sh
@@ -9,7 +9,16 @@ if [[ -f ~/.vimrc || -h ~/.vimrc ]];then
 fi
 
 if (cp ~/mynote/myconfig/template/vimrc ~/.vimrc);then
-	echo "updated to my customed config!"
+	echo "vim updated to my customed config!"
 fi
 
 
+#backup original .tmux.conf config
+if [[ -f ~/.tmux.conf || -h ~/.tmux.conf ]];then
+	mv ~/.tmux.conf ~/.tmux.conf.orig
+	echo "original .tmux.conf backed up!"
+fi
+
+if (cp ~/mynote/myconfig/template/tmux.conf ~/.tmux.conf);then
+	echo "tmux updated to my customed config!"
+fi
