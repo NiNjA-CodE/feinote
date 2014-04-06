135eaf450bff531194506d66841dcd192291f64d
Fei Lunzhou <univfei@gmail.com>
2014-04-06 12:22:38 +0800

    add bash network operations artical

--- test/filetest.c
+++ test/filetest.c
@@ -10,27 +10,27 @@
 
 int
 main(void) {
-				int n,fd,k,input;
-				char buf[BUFFSIZE];
-				//char input[10];
+	int n,fd,k,input;
+	char buf[BUFFSIZE];
+	//char input[10];
 
-				fd = open("feilunzhou.txt",O_RDWR) ;
+	fd = open("feilunzhou.txt",O_RDWR) ;
 
-				while ((n = read(fd,buf,BUFFSIZE)) > 0)
-								if (write(STDOUT_FILENO, buf, n) != n)
-												perror("write error");
+	while ((n = read(fd,buf,BUFFSIZE)) > 0)
+					if (write(STDOUT_FILENO, buf, n) != n)
+									perror("write error");
 
-				//memset(&input,'8',10);
-				input=0;
-				printf("please input your number and offset\n");
-				scanf ("%d %d",&input,&k);
+	//memset(&input,'8',10);
+	input=0;
+	printf("please input your number and offset\n");
+	scanf ("%d %d",&input,&k);
 
-				printf("the size of your input string is %d",sizeof(input));
+	printf("the size of your input string is %d",sizeof(input));
 
-				lseek(fd,k,SEEK_SET);
-				write(fd,&input,sizeof(input));
-				if (n < 0)
-								perror("read error");
+	lseek(fd,k,SEEK_SET);
+	write(fd,&input,sizeof(input));
+	if (n < 0)
+					perror("read error");
 
-				exit(0);
+	exit(0);
 }
